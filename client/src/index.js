d68c3b404c296a237ad848bd7a517df7f0f1b36c
ranandhan <ranandhan@yodlee.com>
2019-03-07 19:48:38 +0530

    axios proxy fix in production

--- client/src/index.js
+++ client/src/index.js
@@ -6,7 +6,7 @@ import * as serviceWorker from "./serviceWorker";
 
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:5000";
+// axios.defaults.baseURL = "http://localhost:5000";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
