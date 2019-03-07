a0cb73d87813727239f36785c2d1f7665ef563f7
ranandhan <ranandhan@yodlee.com>
2019-03-07 17:17:23 +0530

    deployment setup

--- config/config.js
+++ config/config.js
@@ -1,6 +1,5 @@
-module.exports = {
-  mongoURIDEV: "mongodb://localhost:27017/devconnector",
-  mongoURI:
-    "mongodb+srv://ramesh:smh00213@cluster0-mbkzj.mongodb.net/devconnector?retryWrites=true",
-  jwtSecret: "secretKey"
-};
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("./config_prod");
+} else {
+  module.exports = require("./config_dev");
+}
